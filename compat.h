640fb97fb771ed81b814fe7fc5c6687f2466492d
lpoulain <loic.poulain@intel.com>
2014-03-17 08:09:32 +0100

    STPK-1433: Fix sigset_t used by ppoll
    This patch adds the correct sigsegt_t kernel definition
    and convert the Bionic sigseg_t before ppoll syscall.
    
    Change-Id: I26fa391f3ab69e671368131bddc992d336bec432
    Signed-off-by: lpoulain <loic.poulain@intel.com>

--- compat.h
+++ compat.h
@@ -7,9 +7,25 @@
 #define EPOLL_CLOEXEC 02000000
 #endif
 
+
+#define _NSIG_KERNEL		64
+
+#ifdef __i386__
+# define _NSIG_BPW_KERNEL	32
+#else
+# define _NSIG_BPW_KERNEL	64
+#endif
+
+#define _NSIG_WORDS_KERNEL	(_NSIG_KERNEL / _NSIG_BPW_KERNEL)
+
+typedef struct {
+	unsigned long sig[_NSIG_WORDS_KERNEL];
+} sigset_kernel_t;
+
+
 static inline int epoll_create1(int flags)
 {
-        return syscall(__NR_epoll_create1, flags);
+	return syscall(__NR_epoll_create1, flags);
 }
 
 
@@ -17,7 +33,19 @@ static inline int ppoll(struct pollfd *fds, nfds_t nfds,
                 const struct timespec *timeout_ts,
                 const sigset_t *sigmask)
 {
-        return syscall(__NR_ppoll, fds, nfds, timeout_ts, sigmask);
+	if (sizeof(sigset_t) < sizeof(sigset_kernel_t)) {
+		/* Translate Bionic sigset_t to Kernel sigset_t */
+		sigset_kernel_t sigmask_k;
+
+		memset(&sigmask_k, 0, sizeof(sigset_kernel_t));
+		memcpy(&sigmask_k, sigmask, sizeof(sigset_t));
+
+		return syscall(__NR_ppoll, fds, nfds, timeout_ts, &sigmask_k,
+				sizeof(sigset_kernel_t));
+	} else {
+		return syscall(__NR_ppoll, fds, nfds, timeout_ts, sigmask,
+				sizeof(sigset_t));
+	}
 }
 
 #endif
