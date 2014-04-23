9425c5574b950dd75c1990fee48b70b6f984aac0
Samuel Ortiz <sameo@linux.intel.com>
2014-04-23 12:31:36 +0200

    STPK-557: Do not generate static libbluez library
    And avoid any potential GPL violations as anyone could potentially
    statically link with libbluez.
    
    Jianxun: Conflict resolved on Gmin.
    
    Change-Id: Ica784e3ec74d104864a82f24147e9f484070bebb
    Depends-Change-Id: I17007c7906b58e1cf5654db2a58196d7391e62fc
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- Android.mk
+++ Android.mk
@@ -3,17 +3,17 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hcitool
 LOCAL_SRC_FILES := hcitool.c
+LOCAL_SRC_FILES += lib/bluetooth.c lib/hci.c lib/sdp.c
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS) $(LOCAL_PATH)/lib
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libbluez
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hciconfig
 LOCAL_SRC_FILES := hciconfig.c csr.c
+LOCAL_SRC_FILES += lib/bluetooth.c lib/hci.c lib/sdp.c
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS) $(LOCAL_PATH)/lib
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libbluez
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -26,10 +26,10 @@ LOCAL_SRC_FILES := monitor/main.c monitor/mainloop.c \
 		monitor/lmp.c  monitor/l2cap.c \
 		monitor/uuid.c monitor/sdp.c \
 		monitor/crc.c monitor/ll.c
+LOCAL_SRC_FILES += lib/bluetooth.c lib/hci.c lib/sdp.c
 LOCAL_CFLAGS += -DVERSION=\"5.10\"
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS) $(LOCAL_PATH)/lib
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libbluez
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -39,17 +39,12 @@ LOCAL_SRC_FILES := hciattach.c hciattach.h \
 		hciattach_st.c hciattach_ti.c \
 		hciattach_tialt.c hciattach_ath3k.c \
 		hciattach_qualcomm.c hciattach_intel.c
+LOCAL_SRC_FILES += lib/bluetooth.c lib/hci.c lib/sdp.c
 LOCAL_C_INCLUDES := $(KERNEL_HEADERS) $(LOCAL_PATH)/lib
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libbluez
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := libbluez
-LOCAL_SRC_FILES := lib/bluetooth.c lib/hci.c lib/sdp.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/lib
-include $(BUILD_STATIC_LIBRARY)
 
 HCITOOLS_GPL_SRC_TARGET: $(HOST_OUT_EXECUTABLES)/pigz
 	@echo "Copying hcitools source code to system/vendor/gpl"
